09851df5b1d56971b6b09be14dc6a2ffddfd2342
Kaue Freitas <kaue.freitas@pearson.com>
2018-03-02 12:44:09 -0300

    ajustes para remover o que não precisa

--- run.sh
+++ run.sh
@@ -5,12 +5,8 @@ MIGRATION_PATH="./migrations"
 if [ ! -d "$MIGRATION_PATH" ]; then
     # Python Miration Init
     /usr/local/bin/python3.5 /data/manage.py db init
-    rm -rf /data/migrations/env.py
-    cp /data/env.py /data/migrations/
     /usr/local/bin/python3.5 /data/manage.py db migrate
 else
-    rm -rf /data/migrations/env.py
-    cp env.py /data/migrations/
     /usr/local/bin/python3.5 manage.py db migrate
 fi
 
